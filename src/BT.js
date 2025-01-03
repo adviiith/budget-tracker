e15ab6b403a97216249dc47236f8b0f7f8c46d0c
adviiith <advithsharma18@gmail.com>
2025-01-03 13:11:04 +0530

    ch

--- src/BT.js
+++ src/BT.js
@@ -7,6 +7,7 @@ import Papa from 'papaparse';
 import 'jspdf-autotable'; // Import the jsPDF autoTable plugin
 import "./BT.css";
 
+
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ user, children }) => {
   // If the user is not authenticated, redirect to login
   if (!user) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/" />;
   }
 
   // If the user is authenticated, render the children (MyToolsPage)
