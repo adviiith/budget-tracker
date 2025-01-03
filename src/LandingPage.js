36021ce9466346af695813bbd6f160a1b599a9d5
adviiith <advithsharma18@gmail.com>
2025-01-03 13:23:40 +0530

    cu

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./LandingPage.css";
 import { useNavigate, Link } from "react-router-dom";
-import Navbar from "./NB";
+import Navbar from "./Navbar";
 
 const LandingPage = () => {
 
